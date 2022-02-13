1f87183b46022236edaa3a1e29b5bba37669092f
Rostam Boroumand Rad <r66@umbc.edu>
2022-02-13 15:45:48 -0500

    Hopefully this commits the project correctly

---
+++ hello/test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h> 
+#include <string.h>
+#include <errno.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+
+#define __NR_hello 441
+
+long hello_syscall(void) {
+	return syscall(__NR_hello);
+}
+
+int main(int argc, char *argv[]) {
+	long rv;
+
+	rv = hello_syscall();
+
+	if(rv < 0) {
+		perror("Hello syscal failed");
+	}
+	else{
+		printf("Hllo syscall ran successfully, check dmesg output\n");
+	}
+	return 0;
+}
